d814c927618f4a6199afec84452996a7d182f432
Ayush Sharma <32835977+ayushsha1254@users.noreply.github.com>
2023-09-16 01:19:47 +0530

    cors error

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-const express=require('express');
-const app=express();
-const port=5000;
-const mongoose=require('mongoose');
-const {mongoUrl}=require('./keys')
-const cors=require('cors')
-const path=require('path');
-const__dirname=path.resolve();
+const express = require("express");
+const app = express();
+const port = 5000;
+const mongoose = require("mongoose");
+const { mongoUrl } = require("./keys");
+const cors = require("cors");
+const path = require("path");
+const__dirname = path.resolve();
 
-
-
-require('./models/model')
-app.use(express.json())
-app.use(require('./routes/auth'))
-mongoose.connect(mongoUrl)
 app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5000",
-      "*",
-      "https://twitter-clone-mern-sqtg.vercel.app/",
-    ],
-  })
-);
-require('./models/post')
-app.use(require("./routes/createPost"))
-app.use(require('./routes/user'))
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:5000",
+        "*",
+        "https://twitter-clone-mern-sqtg.vercel.app/",
+      ],
+    })
+  );
+  
+require("./models/model");
+app.use(express.json());
+app.use(require("./routes/auth"));
+mongoose.connect(mongoUrl);
 
-app.get('*',function(_,res){
-    res.status(404).json({message:"Route Not Found"})
-})
+require("./models/post");
+app.use(require("./routes/createPost"));
+app.use(require("./routes/user"));
 
+app.use(express.static(path.join(__dirname, "./client/built")));
 
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./client/built/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
 
-mongoose.connection.on("connected",()=>{
-    console.log("successfully connected to mongo")
-})
+mongoose.connection.on("connected", () => {
+  console.log("successfully connected to mongo");
+});
 
-mongoose.connection.on("error",()=>{
-    console.log("not connected to mongodb")
-})
+mongoose.connection.on("error", () => {
+  console.log("not connected to mongodb");
+});
 
-
-app.listen(port,()=>{
-    console.log("Server running on port "+port)
-})
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
