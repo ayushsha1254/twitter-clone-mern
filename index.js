41b086df7befb20bfaef6ae52df0a866bb869024
Ayush Sharma <32835977+ayushsha1254@users.noreply.github.com>
2023-09-16 01:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,14 @@ app.use(
       ],
     })
   );
-  
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://twitter-clone-mern-sqtg.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 require("./models/model");
 app.use(express.json());
 app.use(require("./routes/auth"));
